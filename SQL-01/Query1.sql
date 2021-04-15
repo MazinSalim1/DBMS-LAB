3a1991b18ccc2d57e96a9fe470c562851679d08a
MazinSalim1 <71492673+MazinSalim1@users.noreply.github.com>
2021-04-15 14:16:21 +0530

    Add files via upload

---
+++ SQL-01/Query1.sql
@@ -0,0 +1,7 @@
+insert into participated values('A01','KA052250',11,10000);
+insert into participated values('A02','KA053408',12,50000);
+insert into participated values('A03','KA095477',13,25000);
+insert into participated values('A04','KA031181',14,3000);
+insert into participated values('A05','KA041702',15,5000);
+commit;
+select * from participated;
