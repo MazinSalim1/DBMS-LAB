b656c69ccaac03e0a60843beeac60ae6725f59cc
MazinSalim1 <71492673+MazinSalim1@users.noreply.github.com>
2021-05-27 09:46:48 +0300

    Add files via upload

---
+++ SQL-01/Car.sql
@@ -0,0 +1,4 @@
+use INSURANCE;
+SELECT COUNT(report_num) FROM car, participated 
+WHERE car.reg_num = participated.reg_num
+AND model='Lancer';
---
+++ SQL-01/Update.sql
@@ -0,0 +1,10 @@
+use INSURANCE;
+UPDATE participated
+SET damage_amount=25000
+WHERE report_num=12;
+
+
+insert into accident values('16','2020-05-06','bms Road');
+select * from accident;
+
+
