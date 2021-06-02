48d4b5a77ad20c30c4464c66427b398f3dcefcec
MazinSalim1 <71492673+MazinSalim1@users.noreply.github.com>
2021-06-02 22:24:18 +0300

    Add files via upload

---
+++ SQL-03/LAB3_Query4.sql
@@ -0,0 +1,10 @@
+
+use Suppliers;
+
+select s.sname from supplier s 
+where not exists(
+select p.pid from parts p 
+where not exists(
+select c.sid from catalog c 
+where c.sid = s.sid 
+and c.pid = p.pid));
