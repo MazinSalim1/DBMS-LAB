83e8e27a638cce8482d2e725fee7e121d48cb257
MazinSalim1 <71492673+MazinSalim1@users.noreply.github.com>
2021-07-07 20:13:59 +0300

    Delete SQL-07 directory

--- SQL-07/LAB7_Query5.sql
+++
@@ -1,7 +0,0 @@
-use book_database;
-
-delete from book where book_id = 3;
-select * from book;
-select * from book_authors;
-select * from book_copies;
-select * from book_lending;
--- SQL-07/LAB7_Query6.sql
+++
@@ -1,5 +0,0 @@
-use book_database;
-
-create view view1 
-as select pub_year from book; 
-select * from view1;
