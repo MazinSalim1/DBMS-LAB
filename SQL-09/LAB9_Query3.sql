15654a5d2e9426878c366dd16c14f10cd31b5c45
MazinSalim1 <71492673+MazinSalim1@users.noreply.github.com>
2021-07-07 20:14:08 +0300

    Delete SQL-09 directory

--- SQL-09/LAB9_Query3.sql
+++
@@ -1,3 +0,0 @@
-select m.mov_title from movies m, director d 
-where m.dir_id=d.dir_id 
-and d.dir_name='Alfred Hitchcock';
--- SQL-09/LAB9_Query6.sql
+++
@@ -1,5 +0,0 @@
-select mov_title, max(rev_stars) 
-from movies inner join rating using (mov_id) 
-group by mov_title 
-having max(rev_stars)>0 
-order by mov_title;
